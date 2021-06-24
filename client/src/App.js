e70302f25f050cfbc9d73c25ecc2fc5d43b33112
creaseaj <adam@coreblue.co.uk>
2021-06-24 15:09:46 +0100

    Tailwind installed, displays posts in a grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,37 @@
 import React,{useState, useEffect} from 'react';
-import './App.css';
 
 const App = () => {
-    const [data, setData] = useState({ hits: [] });
+    const [data, setData] = useState([]);
+    const [atad, setAtad] = useState([]);
+
+
     useEffect(async() => {
-        loadData();
+        //loadData();
+            fetch('http://localhost/api/posts')
+  .then(response => response.json())
+  .then(data => setAtad(data));
       }, [])
-    const loadData = async() => {
-        const response = await fetch('http://localhost/api/posts');
-        const data = await response.json();
-        setData(data);
+
+    const Post = ({author,title,body}) => {
+        return(
+            <div class="m-4 py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                <div>
+                    <h2 class="text-gray-800 text-3xl font-semibold">{title}</h2>
+                    <p class="mt-2 text-gray-600">{body}</p>
+                </div>
+                <div class="flex justify-end mt-4">
+                    <p  class="text-xl font-medium text-indigo-500">{author}</p>
+                </div>
+            </div>
+        )
     }
-    console.log(data[0]['id']);
       return (
-    <p>Test {data[0]['id']}</p>
-  );
+          <div class="grid grid-cols-3">
+          {atad.map((r,id) => (
+                <Post author={r.author.name} title={r.title} body={r.body}/>
+          ))}
+          </div>
+          )
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,8 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App.js';
-class Trial extends React.Component {
-    constructor(props) {
-        super(props);
-        this.props = {
-            times: 0,
-            posts: [],
-        }
+import './styles/tailwind.css';
 
-    }
-    render () {
-        fetch('http://localhost/api/posts').then(response => response.json()).then(data => console.log(data[0]));;
-
-        return(<p>Test {this.props.times}</p>)
-    }
-}
   // ========================================
 
   ReactDOM.render(
